ddfbb8402696471e0997c36adbbcdf77015f33f5
Matteo Berchier <maberchier@gmail.com>
2020-11-02 02:03:07 +0100

    Third test

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+# README for PyPi
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='loyverse',
       version='0.0.1',
@@ -16,4 +22,6 @@ setup(name='loyverse',
       ],
       include_package_data=True,
       zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       )
