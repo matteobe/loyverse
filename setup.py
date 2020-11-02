83b7d010332c15076cc6185b001b4e58d19e80b2
Matteo Berchier <maberchier@gmail.com>
2020-11-02 02:07:24 +0100

    4th try

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='loyverse',
-      version='0.0.1',
+      version='0.0.2',
       description='Loyverse API wrapper',
       url='https://github.com/matteobe/loyverse',
       packages=[
