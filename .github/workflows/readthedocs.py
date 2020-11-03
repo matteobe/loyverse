88e32414b9418394b6e37debd68d03849e008d34
Matteo Berchier <maberchier@gmail.com>
2020-11-03 23:50:15 +0100

    ReadTheDocs - Final test

--- .github/workflows/readthedocs.py
+++ .github/workflows/readthedocs.py
@@ -5,6 +5,7 @@ Trigger ReadTheDocs documentation build and check response for confirmation
 import os
 import requests
 
+
 URL = 'https://readthedocs.org/api/v3/projects/loyverse/versions/latest/builds/'
 TOKEN = os.getenv('READTHEDOCS_TOKEN')
 HEADERS = {
@@ -18,8 +19,14 @@ def main():
     response = response.json()
 
     if response['triggered']:
+        print('ReadTheDocs build successfully triggered')
+        print(f"Branch: {response['version']['identifier']}")
+        print(f"Version: {response['build']['version']}")
+        print(f"Build id: {response['build']['id']}")
+        print(f"Triggered: {response['triggered']}")
         return 0
     else:
+        print('ReadTheDocs build failed')
         return 1
 
 
